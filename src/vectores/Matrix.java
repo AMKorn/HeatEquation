2a17c6168d525e16313e6124ca3b7fa446ff75c2
AMKorn <andreas.korn98@gmail.com>
2019-05-29 16:23:23 +0200

    Added a few of algebraic operations with vectors and matrixes

--- src/vectores/Matrix.java
+++ src/vectores/Matrix.java
@@ -11,18 +11,68 @@ package vectores;
  */
 public class Matrix {
 
-    Vector[] Matrix;
+    public Vector[] Matrix;
+    public int lengthI;
+    public int lengthJ;
 
     public Matrix(Vector[] mat) {
         Matrix = mat;
     }
 
     public Matrix(double[][] matrix) {
-        Vector[] tempMatrix = new Vector[matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
+        this.lengthI = matrix.length;
+        Vector[] tempMatrix = new Vector[lengthI];
+        for (int i = 0; i < lengthI; i++) {
+            lengthJ = matrix[i].length;
             tempMatrix[i] = new Vector(matrix[i]);
         }
         Matrix = tempMatrix;
+    }
+    
+    static Matrix matrixMul(Matrix A, Matrix B) throws AlgebraException {
+        Matrix result = new Matrix(new double[A.lengthI][B.lengthJ]);
+        Vector aux = new Vector(new double[B.lengthI]);
+ 
+        for (int j = 0; j < B.lengthJ; j++) {
+ 
+            for (int k = 0; k < B.lengthI; k++)
+                aux.setValue(k, B.getValue(k,j));
+ 
+            for (int i = 0; i < A.lengthI; i++)
+                result.setValue(i, j, A.Matrix[i].dotProduct(aux));
+        }
+        return result;
+    }
+    
+//    static Matrix matrixMul(Matrix A, Matrix B) {
+//        double[][] result = new double[A.lengthI][B.lengthJ];
+//        
+//        for(int j = 0; j < A.lengthJ; j++){
+//            double[] prod = new double[];
+//            double suma;
+//            
+//            for(int i = 0; i < A.lengthI; i++){
+//                suma = A.getValue(i, j)+B.getValue(j, i);
+//                prod += suma;
+//            }
+//        }
+//        return new Matrix(result);
+//    }
+    
+    public double getValue(int i, int j){
+        return Matrix[i].Vector[j];
+    }
+    
+    public void setValue(int i, int j, double value){
+        Matrix[i].Vector[j] = value;
+    }
+    
+    public Vector getRow(int i){
+        return Matrix[i];
+    }
+    
+    public void setRow(int i, Vector vec){
+        Matrix[i] = vec;
     }
 
     @Override
--- src/vectores/Vector.java
+++ src/vectores/Vector.java
@@ -10,20 +10,52 @@ package vectores;
  * @author Andreas
  */
 public class Vector {
-    
+
     double[] Vector;
-    
-    public Vector(double[] vec){
+    int length;
+
+    public Vector(double[] vec) {
         Vector = vec;
+        length = vec.length;
     }
     
+    public double getValue(int i){
+        return Vector[i];
+    }
+    
+    public void setValue(int i, double value){
+        Vector[i] = value;
+    }
+
+    public double dotProduct(Vector b) throws AlgebraException {
+        if (this.Vector.length != b.Vector.length) {
+            throw new AlgebraException("La longitud de los dos vectores es diferente");
+        }
+        double dotProduct = 0;
+        for (int i = 0; i < this.Vector.length; i++) {
+            dotProduct += this.Vector[i]*b.Vector[i];
+        }
+        return dotProduct;
+    }
+    
+    public double dotProduct(Vector a, Vector b) throws AlgebraException {
+        if (a.Vector.length != b.Vector.length) {
+            throw new AlgebraException("La longitud de los dos vectores es diferente");
+        }
+        double dotProduct = 0;
+        for (int i = 0; i < this.Vector.length; i++) {
+            dotProduct += a.Vector[i]*b.Vector[i];
+        }
+        return dotProduct;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         String s = "";
         s += "[";
-        for(int i = 0; i < Vector.length; i++){
+        for (int i = 0; i < Vector.length; i++) {
             s += Vector[i];
-            if(i+1 != Vector.length){
+            if (i + 1 != Vector.length) {
                 s += ", ";
             }
         }
